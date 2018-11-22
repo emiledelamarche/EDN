ab288167dd1cb5ad19179a1d150d0b084b0ae72c
delamarche <emile.delamarche@epitech.eu>
2018-11-22 02:27:10 +0100

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+//
+//  hello_world.c
+//  hello_world.sw
+//
+//  Created by emile delamarche on 09/11/2018.
+//  Copyright © 2018 emile delamarche. All rights reserved.
+//
+
+#include "hello_world.h"
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
